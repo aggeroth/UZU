84a732153ad42d2df93bf22ab881cd49658d7c73
mnakano <minoru.nakano@gmail.com>
2013-05-01 14:38:22 -0700

    UI with Action Bar

--- Uzu/src/com/example/uzu/MainActivity.java
+++ Uzu/src/com/example/uzu/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.uzu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +18,24 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+			case R.id.menu_scan:
+				Toast.makeText(this,"Scan selected", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.menu_drop:
+				Toast.makeText(this,"Drop selected", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.menu_collection:
+				Toast.makeText(this,"Collection selected", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;	
+		}
 		return true;
 	}
 
