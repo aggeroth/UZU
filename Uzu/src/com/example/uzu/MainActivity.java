758455556e72c33a3b58d3e5813a012120c3fb15
Merisha <Merisha@142.232.130.158>
2013-05-01 17:43:28 -0700

    added the animation

--- Uzu/src/com/example/uzu/MainActivity.java
+++ Uzu/src/com/example/uzu/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.uzu;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	@SuppressLint("CutPasteId")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final ImageView iView = (ImageView)findViewById(R.id.imageButton1);
+		
+		final Animation an = AnimationUtils.loadAnimation(this, R.anim.spin);
+		
+		ImageButton imgBtn = (ImageButton) findViewById(R.id.imageButton1);
+
+		
+		imgBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				iView.startAnimation(an);
+			}
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
