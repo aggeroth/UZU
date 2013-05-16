a2977dd80f14a10ba389396aa4e35e71860bf19f
Merisha <Merisha@142.232.130.155>
2013-05-15 17:03:19 -0700

    added comments to ItemActivity

--- UzuSwipe/src/com/example/uzuswipe/ItemActivity.java
+++ UzuSwipe/src/com/example/uzuswipe/ItemActivity.java
@@ -11,13 +11,26 @@ import android.support.v4.view.ViewPager;
 
 import android.view.View;
 import android.widget.Button;
-
+/**
+ * This class grabs the CollectionFragment class list and individually puts them 
+ * in a fragment to swipe.
+ * 
+ * @author Merisha Shim
+ *
+ */
 
 public class ItemActivity extends FragmentActivity {
+	
+	/** This is a MyPageAdapter that is used for swiping. */
 	MyPageAdapter pageAdapter;
 
+	/** This is a string that contains the subjectTitle. */
 	String subjectTitle;
+	
+	/** This is a string that contains the subjectContent. */
 	String subjectContent;
+	
+	/** This is a button for the buttonDelete. */
 	Button buttonDelete;
 
 	@Override
@@ -28,26 +41,31 @@ public class ItemActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_views);
 		// Grabs the position of the page from the previous page
-		final int prePosition = getIntent().getIntExtra("position", 0);
-		
+		final int currentPosition = getIntent().getIntExtra("position", 0);
+						
 		// Creates an array of fragments from CollectionFragment
 		final UzuFragment[] fragments = CollectionFragment.getUzuFragements();
-		
+
 		pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
 
 		final ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
 		pager.setAdapter(pageAdapter);
-		
+		pager.setCurrentItem(currentPosition);
 				
 		// Set buttonDrop
 		buttonDelete = (Button) findViewById(R.id.button1);
 		buttonDelete.setOnClickListener(new View.OnClickListener() {
-			
+				
 			@Override
 			public void onClick(View arg0) {
 			
-				db.deleteUzu(fragments[prePosition]);
-			
+				//deletes the fragment
+				db.deleteUzu(fragments[currentPosition]);
+				
+				//goes back to the previous page 'CollectionFragment'
+				finish();
+				
+				
 			}
 			
 		});
@@ -55,19 +73,36 @@ public class ItemActivity extends FragmentActivity {
 		
 	}
 
+	/**
+	 * This is an inner class that allows the same layout to view different fragments.
+	 * @author Merisha Shim
+	 *
+	 */
 	private class MyPageAdapter extends FragmentPagerAdapter {
+		/** An array of UzuFragments. */
 		private UzuFragment[] fragments;
 
+		/**
+		 * Constructor that takes a FragmentManager, UzuFragment array.
+		 * 
+		 * @param fm
+		 * @param fragments
+		 */
 		public MyPageAdapter(FragmentManager fm, UzuFragment[] fragments) {
 			super(fm);
 			this.fragments = fragments;
 		}
 
+		/**
+		 * This function gets the item from the position of the fragment.
+		 */
 		@Override
 		public Fragment getItem(int position) {
 			return this.fragments[position];
 		}
-
+		/**
+		 * This function returns the number of fragments.
+		 */
 		@Override
 		public int getCount() {
 			return this.fragments.length;
