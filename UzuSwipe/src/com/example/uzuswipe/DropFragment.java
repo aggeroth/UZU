cce2ea49a3f0b5c44aa642d3864f4a3a2f6dca5a
mnakano <minoru.nakano@gmail.com>
2013-05-15 21:37:01 -0700

    image uploading works?

--- UzuSwipe/src/com/example/uzuswipe/DropFragment.java
+++ UzuSwipe/src/com/example/uzuswipe/DropFragment.java
@@ -2,7 +2,7 @@ package com.example.uzuswipe;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Blob;
+import java.nio.ByteBuffer;
 import java.util.Calendar;
 
 import org.apache.http.HttpEntity;
@@ -18,16 +18,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.location.Location;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -46,7 +56,10 @@ public class DropFragment extends Fragment {
 	
 	Button buttonDrop;
 	TextView resultText;
-	static final String url = "http://aggeroth.com:8080/RestEasyServices/ocean/drop";
+	Button buttonImageFunctionality;
+	ImageView uzuImage;
+	private final static int IMAGE_COMPRESSION_RATIO = 400;
+	private static final String SERVER_URL = "http://aggeroth.com:8080/RestEasyServices/ocean/drop";
 	
 	EditText subjectField;
 	EditText textField;
@@ -76,6 +89,38 @@ public class DropFragment extends Fragment {
 		resultText = (TextView)view.findViewById(R.id.result_text);
 		subjectField = (EditText)view.findViewById(R.id.input_subject);
 		textField = (EditText)view.findViewById(R.id.input_text);
+		uzuImage = (ImageView)view.findViewById(R.id.image_uzu_drop);
+		
+		/**
+		 * Allows for the creation of a button that can be used to insert an image into the Uzu item.
+		 * Button can be used to remove image from the Uzu item.
+		 * See onClick() for functionality logic.
+		 */
+		buttonImageFunctionality = (Button) view.findViewById(R.id.button_add_image);
+		
+		/**
+		 * Setting the listener for the buttonImageFunctionality button.
+		 * Allows for the button to both insert and remove an image from the Uzu item.
+		 * The inserting of the image is used to trigger the native Android image gallery for image selection.
+		 * The removing of the image sets the ImageView to null, and resets the button label.
+		 */
+		buttonImageFunctionality.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (buttonImageFunctionality.getText() == activity.getResources().getString(R.string.button_add_image)) {
+					Intent intent = new Intent();
+					intent.setType("image/*");
+					intent.setAction(Intent.ACTION_GET_CONTENT);
+					startActivityForResult(Intent.createChooser(intent, "Select Picture"),1);
+				} else {
+					uzuImage.setImageBitmap(null);
+					uzuImage.setVisibility(View.GONE);
+					// Set new label for button, basically switch from removing an image to adding an image.
+		            buttonImageFunctionality.setText(activity.getResources().getString(R.string.button_add_image));
+				}
+			}
+		});
 		
 		//Initialize Days, Hours and Minutes NumberPicker
 		days = (NumberPicker) view.findViewById(R.id.numberPickerDay);
@@ -136,15 +181,21 @@ public class DropFragment extends Fragment {
 					item.setLatitude((float)loc.getLatitude());
 					item.setLongitude((float)loc.getLongitude());
 					item.setLife(life);
-					//image could be null.
-					item.setImage(null);
+					
+					if(uzuImage.getDrawable() != null){
+						// Get ViewImage's bitmap to convert to byte array.
+						Bitmap uzuBitmap = ((BitmapDrawable)uzuImage.getDrawable()).getBitmap();
+						item.setImage(getImageByteArray(uzuBitmap));
+					} else {
+						item.setImage(null);
+					}
 		
 					Log.d("UZU", "point 3");
 					//Create JSONObject from the Uzu item.
 					JSONObject newItem = createJSON(item);
 					Log.d("UZU", "point 4: " + newItem.toString());
 					//Construct the UzuDropService with URL and the new Uzu item.
-					UzuDropService uzuDrop = new UzuDropService(url, newItem);
+					UzuDropService uzuDrop = new UzuDropService(SERVER_URL, newItem);
 					Log.d("UZU", "point 5");
 					subject = "";
 					text = "";
@@ -179,13 +230,36 @@ public class DropFragment extends Fragment {
 		    object.put("messageBody", item.getMessage());
 		    object.put("birth", (Calendar)item.getBirth());
 		    object.put("death", (Calendar)item.getDeath());
-		    object.put("image", (byte[])item.getImage());
+		    
+		    if(uzuImage.getDrawable() != null){
+			    //object.put("image", (String)item.getImage());
+			    // From http://stackoverflow.com/q/12998918/1243372
+			    object.put("image", Base64.encodeToString(item.getImage(), Base64.URL_SAFE));
+		    } else {
+		    	object.put("image", null);
+		    }
+		    
 		    object.put("life", (Integer)item.getLife());
 		    object.put("categoryID", (Integer)item.getCategoryID());
 		  } catch (JSONException e) {
 		    e.printStackTrace();
 		  }
 		  return object;
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (resultCode == android.app.Activity.RESULT_OK) {
+	        if (requestCode == 1) {
+	            Uri curImageUri = data.getData();
+	            String imagePath = getRealPathFromURI(curImageUri);
+	            Bitmap image = BitmapFactory.decodeFile(imagePath);
+	            uzuImage.setImageBitmap(compressImage(image));
+	            uzuImage.setVisibility(View.VISIBLE);
+	            
+	            // Set new label for button, basically switch from adding an image to deleting an image.
+	            buttonImageFunctionality.setText(activity.getResources().getString(R.string.button_remove_image));
+	        }
+	    }
 	}
 	
 	/**
@@ -274,5 +348,53 @@ public class DropFragment extends Fragment {
 			}	
 			buttonDrop.setClickable(true);
 		}
+	}
+	
+	/**
+	 * Convert bitmap image to a byte array.
+	 * @param image
+	 * @return image in byte array form
+	 */
+	private byte[] getImageByteArray(Bitmap image) {
+		int imageBytes = image.getWidth() * image.getHeight() * 4;
+		ByteBuffer buffer = ByteBuffer.allocate(imageBytes);
+		image.copyPixelsToBuffer(buffer);
+		return buffer.array();
+	}	
+	
+	/**
+	 * Image compression at default width of IMAGE_COMPRESSION_RATIO
+	 * @param uncompressed image
+	 * @return compressed image
+	 */
+	private Bitmap compressImage(Bitmap image) {
+		double width = image.getWidth();
+        double height = image.getHeight();
+        double ratio = IMAGE_COMPRESSION_RATIO / width;
+        int newHeight = (int)(ratio * height);
+        return Bitmap.createScaledBitmap(image, IMAGE_COMPRESSION_RATIO, newHeight, false);
+	}
+	
+	/**
+	 * Convert the image URI to the direct file system path of the image file
+	 * @param contentUri image URI
+	 * @return file system path of image
+	 */
+	private String getRealPathFromURI(Uri contentUri) {
+	    String [] projection = {MediaStore.Images.Media.DATA};
+	    
+	    // managedQuery() is deprecated as of 3.0
+	    // found this on http://stackoverflow.com/a/5915670/1243372
+	    String selection = null;
+	    String[] selectionArgs = null;
+	    String sortOrder = null;
+	    
+	    CursorLoader cursorLoader = new CursorLoader(activity, contentUri, projection, selection, selectionArgs, sortOrder);
+	    Cursor cursor = cursorLoader.loadInBackground();
+	    // end of http://stackoverflow.com/a/5915670/1243372
+	    
+	    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	    cursor.moveToFirst();
+	    return cursor.getString(column_index);
 	}
 }
